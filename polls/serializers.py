9587f4c9ca3eda7befb2f747054f1647f99bc8f8
halayons <halayons@unal.edu.co>
2022-02-13 19:09:54 -0500

    segundo

--- polls/serializers.py
+++ polls/serializers.py
@@ -3,15 +3,16 @@ from .models import Question
 from rest_framework import serializers
 
 
-class QuestionSerializer(serializers.Serializer):
-    question_text = serializers.CharField(max_length=200)
-    pub_date = serializers.DateTimeField()
+# class QuestionSerializer(serializers.Serializer):
+#     question_text = serializers.CharField(max_length=200)
+#     poll_name = serializers.CharField(max_length=100)
+#     pub_date = serializers.DateTimeField()
 
-    # DRF serializer.save() calls self.create(self.validated_data)
-    def create(self, validated_data):
-        return Question.objects.create(**validated_data)
+#     # DRF serializer.save() calls self.create(self.validated_data)
+#     def create(self, validated_data):
+#         return Question.objects.create(**validated_data)
 
-# class QuestionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Question
-#         fields = '__all__'
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
